619305c3f4793a5817026063d4431988478d8a90
Fung Hei Man <94099670+fung-hei-man@users.noreply.github.com>
2022-05-10 14:06:28 +0800

    feat: beat histogram

--- BeatLoader.py
+++ BeatLoader.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 
 
 class BeatLoader:
-    def __init__(self, path):
+    def __init__(self, path, print_logs):
         self.path = path
+        self.print_logs = print_logs
         self.audio = None
 
         self.bpm = -1
@@ -14,8 +15,8 @@ class BeatLoader:
 
         # histogram params (6-dim)
         self.beat_hist = []
-        self.nom_first_amp = 0
-        self.nom_second_amp = 0
+        self.first_weight = 0
+        self.second_weight = 0
         self.relative_amp = 0
         self.first_bpm = 0
         self.second_bpm = 0
@@ -29,58 +30,56 @@ class BeatLoader:
         rhythm_extractor = es.RhythmExtractor2013(method="multifeature")
         self.bpm, self.beats, _, _, self.beats_intervals = rhythm_extractor(self.audio)
 
-        print("BPM:", self.bpm)
-        print("Beat positions (sec.):", self.beats)
+        if self.print_logs:
+            print(f'----- Rhythmic Features Starts -----')
+            print("BPM:", self.bpm)
 
     # =================================================================
     #                           Calculations
     # =================================================================
 
     def calculate_beat_histogram(self):
-        self.first_bpm, peak1_weight, peak1_spread, \
-            self.second_bpm, peak2_weight, peak2_spread, \
+        self.first_bpm, self.first_weight, peak1_spread, \
+            self.second_bpm, self.second_weight, peak2_spread, \
             self.beat_hist = \
             es.BpmHistogramDescriptors()(self.beats_intervals)
 
-        self.sum = sum(self.beat_hist)
-        self.nom_first_amp = peak1_weight / self.sum
-        self.nom_second_amp = peak2_weight / self.sum
-
     def get_rhythmic_feats(self):
-        print("First relative amplitude (A0): %0.1f bpm" % self.nom_first_amp)
-        print("Second relative amplitude (A1): %0.1f bpm" % self.nom_second_amp)
-        print("A1/A0 (RA): %0.1f bpm" % self.relative_amp)
-        print("First peak (P1): %0.1f bpm" % self.first_bpm)
-        print("Second peak (P2): %0.1f bpm" % self.second_bpm)
-        print("Histgram sum (SUM): %0.1f bpm" % self.sum)
+        if self.print_logs:
+            print("First amplitude (A0): %0.1f amp" % self.first_weight)
+            print("Second amplitude (A1): %0.1f amp" % self.second_weight)
+            print("A1/A0 (RA): %0.1f " % self.relative_amp)
+            print("First peak (P1): %0.1f bpm" % self.first_bpm)
+            print("Second peak (P2): %0.1f bpm" % self.second_bpm)
+            print(f'----- Rhythmic Features Ends -----')
 
-        return self.nom_first_amp, self.nom_second_amp, self.relative_amp, self.first_bpm, self.second_bpm, self.sum
+        return self.first_weight, self.second_weight, self.relative_amp, self.first_bpm, self.second_bpm
 
     # =================================================================
     #                           Plotting
     # =================================================================
 
     def plot_wave_and_beat(self, title, path):
-        plt.rcParams['figure.figsize'] = (15, 6)
+        plt.figure(figsize=(15, 6))
         plt.plot(self.audio)
         for beat in self.beats:
             plt.axvline(x=beat * 44100, color='red')
         plt.xlabel('Time (samples)')
         plt.title(title)
 
+        plt.savefig(path, bbox_inches='tight', pad_inches=0.25)
         plt.show()
-        plt.savefig(path)
         plt.clf()
 
     def plot_beat_hist(self, title, path):
-        fig, ax = plt.subplots()
+        fig, ax = plt.subplots(figsize=(10, 5))
+        fig.suptitle(title)
         ax.bar(range(len(self.beat_hist)), self.beat_hist, width=1)
         ax.set_xlabel('BPM')
         ax.set_ylabel('Frequency of occurrence')
-        plt.title(title)
-        ax.set_xticks([20 * x + 0.5 for x in range(int(len(self.beat_hist) / 20))])
-        ax.set_xticklabels([str(20 * x) for x in range(int(len(self.beat_hist) / 20))])
+        ax.set_xticks(range(40, 220, 20))
+        ax.set_xticklabels(range(40, 220, 20))
 
-        plt.show()
-        plt.savefig(path)
-        plt.clf()
+        fig.show()
+        fig.savefig(path, bbox_inches='tight', pad_inches=0.25)
+        fig.clf()
--- Dataset.py
+++ Dataset.py
@@ -7,6 +7,10 @@ import matplotlib.pyplot as plt
 from BeatLoader import BeatLoader
 
 
+def detail_mode(path):
+    return path.endswith('classical.00000.wav') or path.endswith('rock.00000.wav')
+
+
 class Dataset:
     def __init__(self):
         self.input_path = 'input/'
@@ -19,13 +23,14 @@ class Dataset:
         # features used
         self.stft = []  # 12-dim
         self.mfcc = []  # 10-dim
-        self.rhythmic_feats = []  # 6-dim
+        self.rhythmic_feats = []  # 5-dim, without sum
         self.pitch_feats = []  # 5-dim
 
         self.load_audios()
 
     def load_audios(self):
         for idx, genre in enumerate(self.categories):
+            print(f'===== Load {genre.capitalize()} Starts =====')
             path = f'{self.input_path}{genre}.00000.wav'
             # for path in glob.glob(f'{self.input_path}{genre}.*.wav'):
             y, sr = librosa.load(path=path, duration=30)
@@ -33,19 +38,20 @@ class Dataset:
             self.audios.append(y)
             self.labels.append(idx)
 
-            self.calculate_features(y, path)
-            print(f'Loaded all {genre} audios')
+            self.calculate_features(y, path, genre)
 
-    def calculate_features(self, y, path):
-        # self.calculate_stft(y)
-        # self.calculate_mfcc(y)
+            print(f'===== Load {genre.capitalize()} Ends =====')
+
+    def calculate_features(self, y, path, genre):
+        self.calculate_stft(y)
+        self.calculate_mfcc(y)
         beat_loader = self.calculate_rhythmic_feat(path)
 
         # plot graph for first audio of each genre
-        if path.endswith(".00000.wav"):
-            # self.plot_stft(len(self.audios) - 1, genre)
-            # self.plot_mfcc(len(self.audios) - 1, genre)
-            self.plot_beats(beat_loader)
+        if detail_mode(path):
+            self.plot_stft(len(self.audios) - 1, genre)
+            self.plot_mfcc(len(self.audios) - 1, genre)
+            self.plot_beats(beat_loader, genre)
 
     # =================================================================
     #                           Calculations
@@ -59,12 +65,12 @@ class Dataset:
         self.mfcc.append(mfcc)
 
     def calculate_rhythmic_feat(self, path):
-        beat_loader = BeatLoader(path)
+        beat_loader = BeatLoader(path, detail_mode(path))
         beat_loader.load_beats()
         beat_loader.calculate_beat_histogram()
 
-        rhythmicFeats = beat_loader.get_rhythmic_feats()
-        self.rhythmic_feats.append(rhythmicFeats)
+        rhythmic_feats = beat_loader.get_rhythmic_feats()
+        self.rhythmic_feats.append(rhythmic_feats)
 
         return beat_loader
 
@@ -77,23 +83,24 @@ class Dataset:
         img = display.specshow(data, x_axis=x_axis, y_axis=y_axis, sr=self.sr)
         fig.colorbar(img, ax=ax)
 
-        fig.show()
         fig.savefig(path, bbox_inches='tight', pad_inches=0.25)
+        fig.show()
         fig.clf()
 
     def plot_stft(self, idx, genre):
-        title = f'STFT Sample for {genre.capitalize()} Genre'
+        title = f'STFT Example for {genre.capitalize()} Genre'
         path = f'output/stft/{genre}.png'
         data = librosa.amplitude_to_db(np.abs(self.stft[idx]), ref=np.max)
 
         self.plot_graph(data, title, path, x_axis='time', y_axis='linear')
 
     def plot_mfcc(self, idx, genre):
-        title = f'MFCC Sample for {genre.capitalize()} Genre'
+        title = f'MFCC Example for {genre.capitalize()} Genre'
         path = f'output/mfcc/{genre}.png'
 
         self.plot_graph(self.mfcc[idx], title, path, x_axis='time', y_axis='linear')
 
     def plot_beats(self, beat_loader, genre):
-        beat_loader.plot_wave_and_beat(title=f'Beat Estimation Sample for {genre}', path=f'output/beat/{genre}.png')
-        beat_loader.plot_beat_hist(title=f'Beat Histogram Sample for {genre}', path=f'output/beat_hist/{genre}.png')
+        beat_loader.plot_wave_and_beat(title=f'Beat Estimation Example for {genre.capitalize()}', path=f'output/beat/{genre}.png')
+        beat_loader.plot_beat_hist(title=f'Beat Histogram Example for {genre.capitalize()}', path=f'output/beat_hist/{genre}.png')
+
