6820db63d7d425eec22564e270833e5e1f9c2833
fung-hei-man <94099670+fung-hei-man@users.noreply.github.com>
2022-05-12 13:24:12 +0800

    feat: call all models from main

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 from Dataset import Dataset
 from Train import Train
+from models.AdaBoost import TrainWithAdaBoost
+from models.GMM import TrainWithGMM
 from models.KNeighbours import TrainWithKNeighbors
+from models.NeuralNetwork import TrainWithNeuralNetwork
 from models.SVC import TrainWithSVC
+import time
+
 
 if __name__ == '__main__':
     dataset = Dataset()
     train = Train(dataset)
 
-    # print("=== GMM ===")
-    # gmm_model = TrainWithGMM()
-    # train.train_with_model(gmm_model)
+    print("=== GMM ===")
+    start_time = time.time()
+    gmm = TrainWithGMM(dataset)
+    train.train_with_model(gmm)
+    print(f'>>> Training time: {time.time() - start_time}')
 
-    # print('=== KNeighbors ===')
-    # knn_model = TrainWithKNeighbors(dataset)
-    # train.train_with_model(knn_model)
+    print('=== KNeighbors ===')
+    start_time = time.time()
+    knn = TrainWithKNeighbors(dataset)
+    train.train_with_model(knn)
+    print(f'>>> Training time: {time.time() - start_time}')
+
+    print('=== Neural Network ===')
+    start_time = time.time()
+    nn = TrainWithNeuralNetwork(dataset)
+    train.train_with_model(nn)
+    print(f'>>> Training time: {time.time() - start_time}')
 
     print('=== SVM ===')
+    start_time = time.time()
     svm_model = TrainWithSVC(dataset)
     train.train_with_model(svm_model)
+    print(f'>>> Training time: {time.time() - start_time}')
+
+    print('=== AdaBoost ===')
+    start_time = time.time()
+    adaBoost = TrainWithAdaBoost(dataset)
+    train.train_with_model(adaBoost)
+    print(f'>>> Training time: {time.time() - start_time}')
